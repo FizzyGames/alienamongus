445033adb6c338c6d7e792d5f1e197464e4cf41a
Mrfahrenheit94 <Mrfahrenheit94@gmail.com>
2018-09-08 12:57:42 -0400

    added materialize folder fix + more
    Implemented base database main display that scales with players
    connecting

---
+++ AlienAmongUs/Assets/databaseDisplayScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class databaseDisplayScript : MonoBehaviour {
+    public gm gms;
+    public List<GameObject> playerAvatars;
+    public List<GameObject> players;
+    public GameObject playerAvatarPrefab;
+	// Use this for initialization
+	void Start () {
+        playerAvatars = new List<GameObject>();
+        gms = GameObject.FindObjectOfType<gm>();
+  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [ContextMenu("Start Game Display")]
+    public void startGameDisplay()
+    {
+        foreach (playerScript p in gms.allPlayers)
+        {
+            players.Add(p.gameObject);
+            playerAvatars.Add(Instantiate(playerAvatarPrefab));
+            GameObject currentAvatar = playerAvatars[playerAvatars.Count - 1];
+            currentAvatar.transform.SetParent(GameObject.Find("Canvas").transform);
+            currentAvatar.transform.GetChild(0).GetComponent<RawImage>().texture = p.playerPhoto;
+            currentAvatar.transform.GetChild(2).GetComponent<Text>().text = p.playerName;
+        }
+        arrangeAvatars();
+    }
+
+    public void arrangeAvatars()
+    {
+        if (playerAvatars.Count <= 7) { 
+        for (int i = 0; i < playerAvatars.Count; i++)
+        {
+            playerAvatars[i].GetComponent<RectTransform>().anchoredPosition = new Vector3(250 * (i - 3), 125, 0);
+
+
+        }
+    }
+    }
+}
