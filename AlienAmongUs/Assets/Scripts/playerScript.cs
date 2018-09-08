89608cbdaa44b294976b904a5f1abbd4a6cc9c8e
Mrfahrenheit94 <Mrfahrenheit94@gmail.com>
2018-09-08 10:24:52 -0400

    initial player and game managers

--- AlienAmongUs/Assets/Scripts/playerScript.cs
+++ AlienAmongUs/Assets/Scripts/playerScript.cs
@@ -8,14 +8,21 @@ public class playerScript : MonoBehaviour {
 	public string playerName;
 	public Texture playerPhoto;
 	public HappyFunTimes.NetPlayer phoneRef;
+    public gm gms;
+    public bool isAlien, dead, poisoned;
 	// Use this for initialization
 
 	void InitializeNetPlayer(SpawnInfo spawnInfo) {
 		// Save the netplayer object so we can use it send messages to the phone
 		phoneRef = spawnInfo.netPlayer;
-        GameObject.FindObjectOfType<gm>().allNetPlayers.Add(phoneRef);
+        gms = GameObject.FindObjectOfType<gm>();
+        gms.addPlayer(this);
+        phoneRef.RegisterCmdHandler<messageAccuse>("accuse", onAccuse);
+        phoneRef.RegisterCmdHandler<messageSetName>("setName", onSetName);
+        phoneRef.RegisterCmdHandler<messagePoison>("poison", onPoison);
+        phoneRef.RegisterCmdHandler<messageRequestScan>("requestScan", onRequestScan);
 
-	}
+    }
 
 	void Start () {
 		
@@ -25,4 +32,57 @@ public class playerScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private class messageAccuse
+    {
+        public int idToAccuse;
+    };
+
+    private void onAccuse(messageAccuse data)
+    {
+        gms.accusation(ID, data.idToAccuse);
+    }
+
+    private class messageSetName
+    {
+        public string selectedName;
+    };
+
+    private void onSetName(messageSetName data)
+    {
+        playerName = data.selectedName;
+
+        //update the main screen to reflect this
+    }
+
+    private class messagePoison
+    {
+        public int idToPoison;
+    };
+
+    private void onPoison(messagePoison data)
+    {
+        gms.poison(data.idToPoison);
+
+        //update the main screen to reflect this
+    }
+
+    private class messageRequestScan
+    {
+        public int idToScan;
+    };
+
+    private void onRequestScan(messageRequestScan data)
+    {
+        gms.requestID(ID, data.idToScan);
+
+        //update the main screen to reflect this
+    }
+
+    public void death()
+    {
+        dead = true;
+        //PUT UP DAT RED SCREEN
+    }
+
 }
--- AlienAmongUs/Assets/gm.cs
+++ AlienAmongUs/Assets/gm.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 using HappyFunTimes;
 
 public class gm : MonoBehaviour {
-    public List<HappyFunTimes.NetPlayer> allNetPlayers;
-    public List<GameObject> allPlayers;
+    public List<playerScript> allPlayers;
+    public Dictionary<int, playerScript> idToPlayer;
 
 	// Use this for initialization
 	void Start () {
-        allNetPlayers = new List<HappyFunTimes.NetPlayer>();
-        allPlayers = new List<GameObject>();
+        allPlayers = new List<playerScript>();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +19,69 @@ public class gm : MonoBehaviour {
 
     public void gameStart()
     {
+        //assign IDs to every player
+    }
+
+    public void addPlayer(playerScript p)//this is called when a player joins
+    {
+        allPlayers.Add(p);//add them to the list of players
+        idToPlayer.Add(p.ID, p);//and add the ID to the dictionary
+    }
+
+    public void accusation(int source, int target)
+    {
+        if (idToPlayer[target].isAlien)//if the target is an alien
+        {
+            //GAME OVER WE WIN
+
+        }
+        else//if it was a false accusation
+        {
+            kill(source);//kill them for making the mistake
+        }
+    }
+
+    public void poison(int target)
+    {
+        idToPlayer[target].poisoned = true;
+
+    }
+
+    public void requestID(int requester, int target)//this gets the target ID data and returns it to the requester
+    {
+        //update grid to show two people interacting
+        //we need to store who's interacting with who
+        idToPlayer[requester].phoneRef.SendCmd("idDelivery", new messageSendID(target));//THIS IS THE FUNCTION TO SEND A COMMAND TO THE PHONE, idDelivery is what the phone is listening for
 
 
     }
+
+    public void kill(int target)
+    {
+        //update the grid
+        //play the noise
+        idToPlayer[target].death();
+
+    }
+
+    //COMMANDS TO BE SENT TO THE PHONE
+
+    private class messageSendID
+    {
+        public messageSendID(int target)
+        {
+            gm gms = GameObject.FindObjectOfType<gm>();
+            gms.idToPlayer[target].playerName = playerName;
+            gms.idToPlayer[target].playerPhoto = playerPhoto;
+
+        }
+        public string playerName;
+        public Texture playerPhoto;
+
+        //on the phone, the handler will look like
+        //client.addEventListener('idDelivery', function(data){idName = data.playerName}
+
+    }
+
+    //END COMMANDS TO BE SENT TO THE PHONE
 }
